79619c9afe78afde216b7217e13c505c60d2dfde
GonzaRomera <92063887+GonzaRomera@users.noreply.github.com>
2021-10-18 21:40:12 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+let contador, pass, passIngresada: string;
+
+const pass: string = "eureka";
+
+for (let contador: string = 1; contador <= 5; contador++) {
+  console.log("ingrese la contraseña nuevamente");
+  passIngresada = prompt("ingrese una pass");
+  pass == passIngresada;
+  contador = contador + 1;
+}
+console.log("contraseña valida");
